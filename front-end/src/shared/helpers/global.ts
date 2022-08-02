4dd9b554034274cbb3b99c693bdfdee26168517c
Karan Desai <karan.desai12@gmail.com>
2022-08-02 10:39:57 +0530

    remove: unused data declaration

--- front-end/src/shared/helpers/global.ts
+++ front-end/src/shared/helpers/global.ts
@@ -1,5 +1,3 @@
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
 function getFormattedDate(date: Date): string {
   const incomingDateString = new Date(date)
   return incomingDateString.toLocaleString("en-US")
